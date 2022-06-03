701060110ccb57cbd7ceb340c913bd47742f980c
Wiuver Afonso Ribeiro <52933241+Wiuver-Ribeiro@users.noreply.github.com>
2022-06-03 10:01:34 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Índice dos Exercícios</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            <nav class="modulos">
+                <div class="modulo verde">
+                    <h3>Módulo 01</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo laranja">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo verde-escuro">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo vermelho-escuro">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo azul-escuro">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+                <div class="modulo roxo-escuro">
+                    <h3>Módulo 02</h3>
+                    <ul>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="#">Exercício A</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </main>
+    <footer class="rodape">
+        W1uv3r | TI &copy; <?= date('Y'); ?>
+    </footer>
+
+</body>
+
+</html>
